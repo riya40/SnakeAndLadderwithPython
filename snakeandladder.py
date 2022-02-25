c1e1a049799cc325b293f4a550bcc90f355a3bc3
riya40 <priyanunna20@gmail.com>
2022-02-25 22:40:53 +0530

    Added: snake,ladder and win points

--- snakeandladder.py
+++ snakeandladder.py
@@ -1,4 +1,21 @@
-"""Welcome To  Snake and Ladder game"""
+import random
+"""Welcome To  Snake and Ladder game
+Initiating the values for snake ,ladder,and winpoint"""
 
+LADDER = {
+    10: 31,
+    20: 42,
+    45: 78,
+    70: 98
+}
 
+SNAKE = {
+    30: 2,
+    50: 21,
+    68: 46,
+    72: 55,
+    89: 72
+}
+
+WIN_POINT = 100
 
